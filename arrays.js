7134f4245779538ac4f0e24db8d539fba20246bd
Derek Bal <bal.derek.j@gmail.com>
2022-08-18 08:41:17 -1000

    nearing end of Obiwan journey

--- arrays.js
+++ arrays.js
@@ -1,4 +1,33 @@
-/*
-    In this file, we'll be continuing our story
-    with Jon being allowed to flee his fight with Jamie.
-*/
+let backpack = [];
+
+// Obi pack for travail
+backpack.push(
+  'Jedi Robe',
+  'Lightsaber',
+  'Crippling sense of failure and guilt'
+);
+
+// Obi need lightsaber ready, treacherous world
+backpack.splice(1, 1);
+
+// Obi finds a Wookie Coat, perfect for the cold
+backpack.push('Wookie Coat');
+
+// Obi brrrrrr, equips Coat
+backpack.pop();
+
+// ?
+backpack.push(
+  'Blue Milk',
+  'Baby Yoda',
+  'Baby Luke',
+  'Con Device',
+  'High Ground',
+  'Blaster'
+);
+
+// Babies crying, no more room
+let stroller = backpack.splice(3, 2);
+
+console.log(backpack);
+console.log(backpack, stroller);
